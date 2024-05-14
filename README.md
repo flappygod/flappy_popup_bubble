e7c1102b5d7d434c80df71adf9bcacfe117800b8
lilin <327603258@qq.com>
2024-05-14 14:24:16 +0800

    add example

--- README.md
+++ README.md
@@ -11,5 +11,44 @@ add flappy_popup_bubble to your yaml.
 
 ## Usage
 
+# Pop-up Menu Example
+
+Below is an example of how to build a pop-up menu in Flutter using a custom `PopupMenu` widget:
+
+```dart
+/// Build pop-up menu
+Widget _buildPopMenu(Widget child) {
+  return PopupMenu(
+    menusBuilder: (context, controller) {
+      return [
+        PopupMenuBtn(
+          text: "Function One",
+          icon: const Icon(
+            Icons.scale,
+            color: Colors.white,
+            size: 16,
+          ),
+          onTap: () {
+            controller.hide();
+          },
+        ),
+        PopupMenuBtn(
+          text: "Function Two",
+          icon: const Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 16,
+          ),
+          onTap: () {
+            controller.hide();
+          },
+        ),
+      ];
+    },
+    menuHeight: 40,
+    child: child,
+  );
+}
+
 
 
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flappy_popup_bubble/flappy_popup_bubble.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,28 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -55,19 +37,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -89,37 +58,56 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: _buildPopMenu(
+            Container(
+              decoration: BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.circular(8)),
+              width: 140,
+              height: 50,
+              alignment: Alignment.center,
+              child: const Text(
+                "Long Press",
+                style: TextStyle(color: Colors.white, fontSize: 13),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+
+  ///build pop menu
+  Widget _buildPopMenu(Widget child) {
+    return PopupMenu(
+      menusBuilder: (context, controller) {
+        return [
+          PopupMenuBtn(
+            text: "Function One",
+            icon: const Icon(
+              Icons.scale,
+              color: Colors.white,
+              size: 16,
+            ),
+            onTap: () {
+              controller.hide();
+            },
+          ),
+          PopupMenuBtn(
+            text: "Function Two",
+            icon: const Icon(
+              Icons.add,
+              color: Colors.white,
+              size: 16,
+            ),
+            onTap: () {
+              controller.hide();
+            },
+          ),
+        ];
+      },
+      menuHeight: 40,
+      child: child,
+    );
+  }
+
+
 }
